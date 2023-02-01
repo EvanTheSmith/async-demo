0bdc66f026c1acdd287659ba3f2333e60d4bf1cc
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-01 14:56:14 -0600

    attempted making promise syntax variants of getRepositories and gitCommits

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function getUser(id) {
         }, 2000);
     });
 }
+
+function getRepositories(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling Github API ...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling Github API ...');
+            resolve(['commit']);
+        }, 2000);
+    });
+}
