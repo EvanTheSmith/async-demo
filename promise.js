916c71e1dab5f4f412a3daed79ea7b49ee0cf800
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-01 14:40:58 -0600

    added rejection fork in promise

--- promise.js
+++ promise.js
@@ -1,5 +1,9 @@
 const p = new Promise((resolve, reject) => { // generic promise template
-    setTimeout(() =>  { resolve(1); }, 1000); // resolve with value of 1 after 1 second
+    setTimeout(() =>  { 
+        // resolve(1); 
+        reject(new Error('message'));
+    }, 1000); 
 });
 
-p.then(result => console.log('Result', result));
+p.then(result => console.log('Result', result))
+.catch(error => console.log('Error', error.message));
