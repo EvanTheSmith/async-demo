b5c08f5d98d7c0c9e000aeb2c3261229eb477700
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-01 15:54:43 -0600

    testing auto-rejecting promise

--- promise.js
+++ promise.js
@@ -13,4 +13,7 @@ p.then(result => console.log('Result', result))
 // Promises can be immediately resolved or rejected:
 
 const resolved_promise = Promise.resolve({ id: 1 });
+const rejected_promise = Promise.reject({ id: 2 });
+
 resolved_promise.then(result => console.log('Resolved Promise: ', result));
+rejected_promise.catch(result => console.log('Rejected Promise: ', result));
